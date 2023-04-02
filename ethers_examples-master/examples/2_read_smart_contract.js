7e2788e685349e8f162421f4889f24e9b9b3d94f
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-04-02 15:25:35 +0200

    Script to read from smart contracts

--- ethers_examples-master/examples/2_read_smart_contract.js
+++ ethers_examples-master/examples/2_read_smart_contract.js
@@ -1,32 +1,33 @@
-const { ethers } = require("ethers");
+import { ethers } from 'ethers';
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
+const provider = new ethers.providers.AlchemyProvider('mainnet', ALCHEMY_KEY);
 
+//ethersJS allows to store ABI in an array and only store the functions we care about
 const ERC20_ABI = [
-    "function name() view returns (string)",
-    "function symbol() view returns (string)",
-    "function totalSupply() view returns (uint256)",
-    "function balanceOf(address) view returns (uint)",
+  'function name() view returns (string)',
+  'function symbol() view returns (string)',
+  'function totalSupply() view returns (unint256)',
+  'function balanceOf(address) view returns (uint)',
 ];
+// create a JS version of a contract to read information form it
+const usdtContractAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+const contract = new ethers.Contract(usdtContractAddress, ERC20_ABI, provider);
 
-const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
-const contract = new ethers.Contract(address, ERC20_ABI, provider)
+async function main() {
+  const name = await contract.name();
+  const symbol = await contract.symbol();
+  // const totalSupply = await contract.totalSupply();
+  console.log(`\nReading from ${usdtContractAddress}\n`);
+  console.log(`Name: ${name}`);
+  console.log(`Symbol: ${symbol}`);
+  // console.log(`Total Supply: ${totalSupply}\n`);
 
-const main = async () => {
-    const name = await contract.name()
-    const symbol = await contract.symbol()
-    const totalSupply = await contract.totalSupply()
-
-    console.log(`\nReading from ${address}\n`)
-    console.log(`Name: ${name}`)
-    console.log(`Symbol: ${symbol}`)
-    console.log(`Total Supply: ${totalSupply}\n`)
-
-    const balance = await contract.balanceOf('0x6c6Bc977E13Df9b0de53b251522280BB72383700')
-
-    console.log(`Balance Returned: ${balance}`)
-    console.log(`Balance Formatted: ${ethers.utils.formatEther(balance)}\n`)
+  const balance = await contract.balanceOf(
+    '0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503'
+  );
+  console.log(`Balance Returned: ${balance}`);
+  console.log(`Balance Formatted: ${ethers.utils.formatEther(balance)}\n`);
 }
 
-main()
+main();
